570e368f3345d7ebead29e7fba344d8b81da5ae7
KirillK <lirostin@gmail.com>
2026-02-10 21:39:33 +0300

    Описал зависимости через ruff. Перенёс main.py чтобы корректно импортировались локальные зависимости. Добавил Makefile для удобной настройки env

---
+++ main.py
@@ -0,0 +1,22 @@
+# import os
+# import pandas as pd
+from src.data import load_data, split_data
+from src.features import add_time_features
+from src.model import TaxiFareModel
+
+# Глобальная переменная
+DATA_PATH = "data/uber.csv"
+
+if __name__ == "__main__":
+    # Загрузка и обработка данных
+    raw_data = load_data(DATA_PATH)
+    processed_data = add_time_features(raw_data)
+    X_train, X_test, y_train, y_test = split_data(processed_data)
+
+    # Обучение модели
+    model = TaxiFareModel()
+    model.fit(X_train, y_train)
+
+    # Оценка
+    score = model.model.score(X_test, y_test)
+    print(f"R²: {score:.2f}")
